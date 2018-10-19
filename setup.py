4987266c68792a3c6d22a5af96e1656457b4f86e
Trent Hauck <trent@trenthauck.com>
2018-10-18 18:58:53 -0700

    Add version bumping.

--- setup.py
+++ setup.py
@@ -5,47 +5,44 @@
 
 from setuptools import setup, find_packages
 
-with open('README.md') as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
-requirements = ['Click>=6.0', ]
+requirements = ["Click>=6.0"]
 
-setup_requirements = [ ]
+setup_requirements = []
+test_requirements = []
 
-test_requirements = [ ]
+version = "0.1.0"
 
 setup(
     author="Trent Hauck",
-    author_email='trent@trenthauck.com',
+    author_email="trent@trenthauck.com",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     description="GCGC",
-    entry_points={
-        'console_scripts': [
-            'gcgc=gcgc.cli:main',
-        ],
-    },
+    entry_points={"console_scripts": ["gcgc=gcgc.cli:main"]},
     install_requires=requirements,
     license="MIT license",
     long_description=readme,
     include_package_data=True,
-    keywords='gcgc',
-    name='gcgc',
-    packages=find_packages(include=['gcgc']),
+    keywords="gcgc",
+    name="gcgc",
+    packages=find_packages(include=["gcgc"]),
     setup_requires=setup_requirements,
     tests_require=test_requirements,
-    url='https://github.com/tshauck/gcgc',
-    version='0.1.0',
+    url="https://github.com/tshauck/gcgc",
+    version=version,
     zip_safe=False,
 )
