0fa1c4c14bea78eab14da730eea043f47612f820
Trent Hauck <trent@trenthauck.com>
2018-10-18 21:46:02 -0700

    Bumping 0.1.1 to 0.1.2dev from travis.

--- gcgc/__init__.py
+++ gcgc/__init__.py
@@ -1,4 +1,4 @@
 # (c) Copyright 2018 Trent Hauck
 # All Rights Reserved
 
-__version__ = "0.1.1"
+__version__ = "0.1.2dev"
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requirements = ["Click>=6.0"]
 setup_requirements = []
 test_requirements = []
 
-version = "0.1.1"
+version = "0.1.2dev"
 
 setup(
     author="Trent Hauck",
