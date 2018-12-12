25f2169d27da11240327b17e94cf763dc600d840
Trent Hauck <trent@trenthauck.com>
2018-12-11 19:53:12 -0800

    Improve tests

--- gcgc/tests/parser/test_parser.py
+++ gcgc/tests/parser/test_parser.py
@@ -2,13 +2,14 @@
 # All Rights Reserved
 
 import unittest
+from operator import itemgetter
 from pathlib import Path
 
 from Bio.Alphabet import IUPAC
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 
-from gcgc.fields import FileMetaDataField
+from gcgc.fields import FileMetaDataField, AnnotationField
 from gcgc.parser.base import EncodedSeqLengthParser, SequenceParser
 from gcgc.parser.gcgc_record import GCGCRecord
 
@@ -20,20 +21,34 @@ class TestParser(unittest.TestCase):
         f = FileMetaDataField.from_paths("species", vocab)
         ff = [f]
 
+        annotations = {"annotation": "a"}
+        af = AnnotationField.from_annotations(
+            "annotation", [annotations], preprocess=itemgetter("annotation")
+        )
+        afs = [af]
+
         length_parser = EncodedSeqLengthParser(conform_to=10)
 
-        sp = SequenceParser(encapsulate=True, seq_length_parser=length_parser, file_features=ff)
+        sp = SequenceParser(
+            encapsulate=True,
+            seq_length_parser=length_parser,
+            file_features=ff,
+            annotation_features=afs,
+        )
 
         dna = IUPAC.IUPACUnambiguousDNA()
-        input_seq = SeqRecord(Seq("ATCG", alphabet=dna))
+
+        input_seq = SeqRecord(Seq("ATCG", alphabet=dna), annotations=annotations)
 
         test_values = [
-            (input_seq, Path("ecoli"), 0),
-            (input_seq, Path("human"), 1),
-            (input_seq, Path("human"), 1),
+            (input_seq, Path("ecoli"), 0, 0),
+            (input_seq, Path("human"), 1, 0),
+            (input_seq, Path("human"), 1, 0),
         ]
 
-        for i, p, es in test_values:
+        for i, p, es, annotation_label in test_values:
             r = GCGCRecord(path=p, seq_record=i)
             resp = sp.parse_record(r)
+
             self.assertEqual(resp["species"], es)
+            self.assertEqual(resp["annotation"], annotation_label)
