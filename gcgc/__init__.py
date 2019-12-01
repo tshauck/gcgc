209e1e3a9384fd184ccd78111684bf6781d1e9d4
Trent Hauck <trent@trenthauck.com>
2019-12-01 11:45:08 -0800

    Finalize v0.11.0.

--- gcgc/__init__.py
+++ gcgc/__init__.py
@@ -2,7 +2,7 @@
 # All Rights Reserved
 """Top-level GCGC module."""
 
-__version__ = "0.11.0-dev.3"
+__version__ = "0.11.0"
 
 from gcgc.tokenizer import SequenceTokenizer
 from gcgc.tokenizer import SequenceTokenizerSpec
--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def _get_long_description():
 
 setup(
     name="gcgc",
-    version="0.11.0-dev.3",
+    version="0.11.0",
     description=DESCRIPTION,
     long_description=_get_long_description(),
     long_description_content_type="text/markdown",
