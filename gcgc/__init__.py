7e8b69b74307f63854c7fc879a0e00014e4c30a6
Trent Hauck <trent@trenthauck.com>
2020-02-16 09:13:46 -0800

    Bump to 0.12.1-dev.5.

--- gcgc/__init__.py
+++ gcgc/__init__.py
@@ -11,6 +11,6 @@ from gcgc.tokenizer import KmerTokenizerSettings
 from gcgc.tokenizer import SequenceTokenizer
 from gcgc.tokenizer import SequenceTokenizerSettings
 
-__version__ = "0.12.1-dev.4"
+__version__ = "0.12.1-dev.5"
 
 _warnings.simplefilter(action="ignore", category=PendingDeprecationWarning)
--- gcgc/cli.py
+++ gcgc/cli.py
@@ -20,7 +20,7 @@ def cli():
 @cli.command()
 def version():
     """Print the version and exit."""
-    click.echo("0.12.1-dev.4")
+    click.echo("0.12.1-dev.5")
 
 
 @cli.group("tokenizer")
