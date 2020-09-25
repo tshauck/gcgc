f0cf0e6ff94b62feca9d9cb000629c32d8e04c1c
Trent Hauck <trent@trenthauck.com>
2020-09-25 15:12:34 -0700

    build: removes poetry to use setup.py

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,11 @@
 ARG PY_VERSION=3.7
 FROM python:$PY_VERSION
 
-RUN pip install -U pip poetry
+RUN pip install -U pip
 
 WORKDIR /gcgc
 COPY ./ ./
 
-# Install dataclasses if we're on 3.6
-RUN python3.6 -m pip install dataclass || true
-RUN POETRY_VIRTUALENVS_CREATE=false poetry install -E pytorch -E hf
+RUN pip install -e .[dev,hf,pytorch]
 
 WORKDIR /workspace
--- gcgc/__init__.py
+++ gcgc/__init__.py
@@ -2,15 +2,26 @@
 # All Rights Reserved
 """Top-level GCGC module."""
 
-import os
+import sys
 import warnings as _warnings
 
 from gcgc import tokenizer
 from gcgc.tokenizer.kmer_tokenzier import KmerTokenizer
 from gcgc.tokenizer.kmer_tokenzier import SequenceTokenizer
 
-__all__ = ["tokenizer", "KmerTokenizer", "SequenceTokenizer"]
+if sys.version_info[:2] >= (3, 8):
+    # pylint: disable=no-name-in-module
+    from importlib import metadata as importlib_metadata
+else:
+    import importlib_metadata
 
-__version__ = "0.13.0"
+__version__ = importlib_metadata.version("gcgc")
+
+__all__ = [
+    "tokenizer",
+    "KmerTokenizer",
+    "SequenceTokenizer",
+    "__version__",
+]
 
 _warnings.simplefilter(action="ignore", category=PendingDeprecationWarning)
